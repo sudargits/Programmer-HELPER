7f7d3c3343e8d7f0f0d1d9765aeca2a60cc0cd57
Yatno Sudar <yatnosudar@gits.co.id>
2016-06-23 11:28:57 +0700

    Remove User
    Remove User

--- SWIFT/PrefHelper.swift
+++ SWIFT/PrefHelper.swift
@@ -11,31 +11,22 @@ class Pref {
     init(){
     
     }
-    
     func saveObject(key:String ,value:AnyObject){
         pref.setObject(value, forKey: key)
         commit()
     }
-    
     func getToken()->String{
         if let token = pref.objectForKey(PrefConstant.KEY_TOKEN) {
             return token as! String
         }
         return ""
     }
-    
     func getBoolean(key:String)->Bool{
         return pref.boolForKey(key);
     }
-    
     func getObject(key:String)->AnyObject{
          return pref.objectForKey(key)!
     }
-    func getUserModel()->BKMUserData{
-        let dt:BKMUserDataBase = BKMUserDataBase(object: pref.objectForKey("user_data")!)
-        return (dt.content?.userData)!
-    }
-    
     func isEmpty(key:String)->Bool{
         if(pref.objectForKey(key) != nil) {
             return true
